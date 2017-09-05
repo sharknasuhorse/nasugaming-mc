879b69755e794aa3b7717433b2accc09043614e2
sharknasuhorse <naka-ryo6459@nifty.com>
2017-09-05 22:05:13 +0900

    mis

--- start.go
+++ start.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
+	import "github.com/codeskyblue/go-sh"
 )
 
 func main() {
 
 	for {
-		a := exec.Command("ps -ef | grep minecraft_server.1.12.1 | grep -v grep | wc -l")
+		
+		a := sh.Command("ps -ef | grep minecraft_server.1.12.1 | grep -v grep | wc -l").Output()
+		a := sh.Command(ps 
 		fmt.Println(a)
 	}
 
