f0cc5b543573309a478f0d9f499095fb79c43436
sharknasuhorse <naka-ryo6459@nifty.com>
2017-09-05 21:52:26 +0900

    aa

--- start.go
+++ start.go
@@ -1,7 +1,15 @@
 package main
 
-import(
-"fmt"
-"os/exec"
+import (
+	"fmt"
+	"os/exec"
 )
 
+func main() {
+
+	for {
+		a := exec.Command("ps -ef | grep minecraft_server.1.12.1 | grep -v grep | wc -l")
+		fmt.Println(a)
+	}
+
+}
